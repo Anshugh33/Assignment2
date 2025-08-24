c6bb6a694a66644d15a88dfa77300d2b45e8735b
Anshugh33 <ghimireanshu46@gmail.com>
2025-08-24 19:26:17 +0545

    part2.py

--- part2.py
+++ part2.py
@@ -1,3 +1,5 @@
+#incorrect password 
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
@@ -9,5 +11,5 @@ driver.find_element(By.ID,"user-name").send_keys("standard_user")
 driver.find_element(By.ID,"password").send_keys("problem_user")
 driver.find_element(By.ID,"login-button").click()
 driver.find_element(By.XPATH,"//input[@id='error-messege-container']")
-
 time.sleep(7)
+
