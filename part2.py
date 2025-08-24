bc23c11dc7927ae98e24cc2055d7a23049eaf668
Anshugh33 <ghimireanshu46@gmail.com>
2025-08-24 19:05:10 +0545

    part2.py

---
+++ part2.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver=webdriver.Chrome()
+
+driver.get("https://www.saucedemo.com/") 
+driver.find_element(By.ID,"user-name").send_keys("standard_user")
+time.sleep(7)
