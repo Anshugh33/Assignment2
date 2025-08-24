c042d5552259b25013d54b7f29e4fa5bb553ef75
Anshugh33 <ghimireanshu46@gmail.com>
2025-08-24 19:19:22 +0545

    part2.py

--- part2.py
+++ part2.py
@@ -7,4 +7,7 @@ driver=webdriver.Chrome()
 driver.get("https://www.saucedemo.com/") 
 driver.find_element(By.ID,"user-name").send_keys("standard_user")
 driver.find_element(By.ID,"password").send_keys("problem_user")
+driver.find_element(By.ID,"login-button").click()
+driver.find_element(By.XPATH,"//input[@id='error-messege-container']")
+
 time.sleep(7)
